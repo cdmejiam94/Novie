0d88e35555f72e082964e59b2c8096b14738fe7a
Carlos Mejia <cdmejiam@gmail.com>
2020-02-11 16:53:33 -0600

    Splash Screen implemented

--- app/src/main/java/com/preeliminatorylabs/novie/SplashActivity.kt
+++ app/src/main/java/com/preeliminatorylabs/novie/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.preeliminatorylabs.novie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.preeliminatorylabs.novie.view.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private final val SPLASH_TIME_OUT: Long = 1500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },SPLASH_TIME_OUT)
     }
 }
---
+++ app/src/main/java/com/preeliminatorylabs/novie/view/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.preeliminatorylabs.novie.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.preeliminatorylabs.novie.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
